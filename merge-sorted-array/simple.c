fce7c6ae764e655e9b990187f417358838dddd54
hoangtuanictvn <19290570+hoangtuanictvn@users.noreply.github.com>
2023-10-02 13:50:06 +0700

    add: simple merge sorted array

---
+++ merge-sorted-array/simple.c
@@ -0,0 +1,37 @@
+// Simple solution: Combine two arrays in to the one and sort them in decreasing order
+#include <stdio.h>
+
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
+    int i = m;
+    for (;i < nums1Size; i++){
+        nums1[i] = nums2[nums1Size - i - 1];
+    }
+}
+
+
+void sort(int* nums, int numSize){
+    int i, j = 0;
+    for (i = 0; i < numSize; i++) {
+        for (j = i+1; j < numSize; j++) {
+            if (nums[i] > nums[j]) {
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+            }
+        }
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int m = 3, n = 3;
+    int nums1[] = {1,2,3,0,0,0},
+        nums2[] = {2,5,6};
+    merge(nums1, m+n, m, nums2, n, n);
+    sort(nums1, n+m);
+    int i;
+    for (i = 0; i < m+n; i++) {
+        printf("%d\n", nums1[i]);
+    }
+    return 0;
+}
