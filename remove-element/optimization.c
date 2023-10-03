07987cc1545b946afa95d4625b392f88dc41d129
hoangtuanictvn <19290570+hoangtuanictvn@users.noreply.github.com>
2023-10-03 10:55:03 +0700

    chore: optimize memory space

--- remove-element/optimization.c
+++ remove-element/optimization.c
@@ -4,13 +4,10 @@ int removeElement(int* nums, int numsSize, int val){
     int i = 0;
     int k = 0;
     while (i < numsSize) {
-        if(nums[i] == val) {
-            nums[i] = nums[numsSize-1];
-            numsSize--;
-        } else {
-            k ++;
-            i ++;
+        if(nums[i] != val) {
+            nums[k++] = nums[i];
         }
+        i++;
     }
     return k;
 }
